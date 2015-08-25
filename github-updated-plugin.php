6d9fe8612876191547f82a96bc2537538ed2b7b0
CrossRefTech <joshuahammill@gmail.com>
2015-08-24 22:34:27 -0400

    github-updated-plugin.php

---
+++ github-updated-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/**
+* Plugin Name: GitHub Updated Plugin
+* Plugin URI: http://wpcoder.ca/
+* Description: A starter for writing a plugin that's updated using GitHub instead of the WordPress repository.
+* Version: 1.0
+* Author: Joshua Hammill
+* Author URI: http://wpcoder.ca/
+* License: GPL2
+*/

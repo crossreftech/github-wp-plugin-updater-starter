98d924b2ee5bd751ed37c540b4f531e10d557da9
CrossRefTech <joshuahammill@gmail.com>
2015-08-24 22:54:55 -0400

    Update github-updated-plugin.php

--- github-updated-plugin.php
+++ github-updated-plugin.php
@@ -3,7 +3,7 @@
 * Plugin Name: GitHub Updated Plugin
 * Plugin URI: http://wpcoder.ca/
 * Description: A starter for writing a plugin that's updated using GitHub instead of the WordPress repository.
-* Version: 1.0
+* Version: 1.0.1
 * Author: Joshua Hammill
 * Author URI: http://wpcoder.ca/
 * License: GPL2
@@ -18,7 +18,14 @@ add_action('wp_head','hook_css');
 
 function hook_css() {
 
-	$output="<style>.plugins tr { background: #f7f7f7 !important; } </style>";
+	$output="<style>
+		#adminmenu, 
+		#adminmenu .wp-submenu, 
+		#adminmenuback, 
+		#adminmenuwrap {
+	    		background-color: #0e1318 !important;
+		} 
+	</style>";
 
 	echo $output;
 
